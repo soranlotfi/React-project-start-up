95fdb8469fc17f103396a7f34d4f521b7ff17cee
Soran Lotfi <soranit2000@gmail.com>
2023-03-13 22:45:32 +0330

    propTypes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import{useState} from "react";
 import Counter from "./Components/Counter";
 import "./style/style.css"
+import PropTypes from "prop-types";
 const App =  ()=>{
     const [count ,setCount] = useState(50);
     const increaseCount = ()=>{
@@ -17,8 +18,15 @@ const App =  ()=>{
                 <header >
                     <h1>شمارنده ی من </h1>
                 </header>
-               <Counter/>
+               <Counter inc = {increaseCount} dec = {decreaseCount} res={resetCount} count = {count}/>
             </div>
         )
+}
+
+Counter.propTypes={
+    inc:PropTypes.func,
+    dec : PropTypes.func,
+    res : PropTypes.func,
+    count : PropTypes.number
 }
 export  default  App;
--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,25 +1,36 @@
-import {Component} from "react";
+// import {Component} from "react";
 
-class counter extends  Component {
-    changName=()=>{
-            this.setState({name:"sina"})
-    }
-    constructor() {
-        super();
-        this.state={
-            name:"soran"
-        }
-    }
+// class counter extends  Component {
+//     changName=()=>{
+//             this.setState({name:"sina"})
+//     }
+//     constructor() {
+//         super();
+//         this.state={
+//             name:"soran"
+//         }
+//     }
+//
+//
+//     render() {
+//         return(
+//             <div>
+//                 <p>{this.state.name}</p>
+//                 <button onClick={this.changName}> تغیر نام </button>
+//             </div>
+//         )
+//     }
+// }
 
-
-    render() {
-        return(
-            <div>
-                <p>{this.state.name}</p>
-                <button onClick={this.changName}> تغیر نام </button>
-            </div>
-        )
-    }
+const counter = ({inc,dec,res,count})=>{
+    return(
+        <div>
+            <h1>{count}</h1>
+            <button onClick={inc}>افزایش</button>
+            <button onClick={dec}>کاهش </button>
+            <button onClick={res}>بازنشانی </button>
+        </div>
+    )
 }
 
 export default  counter;
